ea9c4694dec0235fff48a4a8f3e013de3e2a7f87
SupeerSanny <ksanny556@gmail.com>
2023-08-05 15:15:00 +0530

    Day - 16 (1732. Find the Highest Altitude)

---
+++ src/FindtheHighestAltitude.java
@@ -0,0 +1,16 @@
+//LeetCode: https://leetcode.com/problems/find-the-highest-altitude/?envType=study-plan-v2&envId=leetcode-75
+import java.util.*;
+public class FindtheHighestAltitude {
+    public static int largestAltitude(int[] gain) {
+        int currAlt = 0, maxAlt = 0;
+        for (int i = 0; i < gain.length; i++) {
+            currAlt += gain[i];
+            maxAlt = Math.max(maxAlt, currAlt);
+        }
+        return maxAlt;
+    }
+    public static void main(String[] args) {
+        int[] gain = {-5,1,5,0,-7};
+        System.out.println(largestAltitude(gain));
+    }
+}
